775b63698d6124050c09fad82c823b9ca4514e25
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-06-09 22:45:53 +0200

    fix: mistyped css

--- frontend/src/assets/wrappers/LandingPage.js
+++ frontend/src/assets/wrappers/LandingPage.js
@@ -4,7 +4,7 @@ const Wrapper = styled.section`
 	nav {
 		width: var(--fluid-width);
 		max-width: var(--max-width);
-		margin: 0 ayuto;
+		margin: 0 auto;
 		height: var(--nav-height);
 		display: flex;
 		align-items: center;
